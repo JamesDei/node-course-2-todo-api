1089eef780278b4635b0ae210c57ab8d9ecc9f48
James Osley <james@MacBook-Pro-de-James.local>
2018-02-06 00:11:48 +0100

    Test Post /todos route

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
-        text: req.body.text,
+        text: req.body.text
     });
 
     todo.save().then((doc) => {
@@ -27,3 +28,5 @@ app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
+
+module.exports = {app};
