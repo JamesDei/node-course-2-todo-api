cee6c8b940af1047782b30810aa6544bafebb95f
James Osley <james@MacBook-Pro-de-James.local>
2018-02-08 00:35:49 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -22,12 +22,13 @@ var UserSchema = new mongoose.Schema({
             message: '{VALUE} is not a valid email'
         }
     },
-    
+     
     password: {
         type: String,
         require: true, 
         minlength: 6,
     },
+
     tokens: [{
         access: {
             type: String,
@@ -76,6 +77,30 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.access': 'auth',
     });
 };
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+
+           
+        });
+   
+    });
+};
+
 
 UserSchema.pre('save', function (next) {
     var user = this;
